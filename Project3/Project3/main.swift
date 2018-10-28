abc4821806291bf3f4be7fc1615c5f9716b10e54
NlionDev <lion.nicolas.dev@gmail.com>
2018-10-28 12:54:57 +0100

    Modification du main.swift en rajoutant la création de l'equipe 2, le choix des noms des joueurs et la création de classe pour chaques armes des personages

--- Project3/Project3/main.swift
+++ Project3/Project3/main.swift
@@ -6,85 +6,140 @@
 //  Copyright © 2018 Nicolas Lion. All rights reserved.
 //
 
+
 // Class Character Initialization
 class Character {
     var characterName: String
     var life = 0
-    var strike = 0
-    var magicPower = 0
-    var weapons: String
     
-    init (characterName: String, weapons: String){
+    init (characterName: String){
         self.characterName = characterName
-        self.weapons = weapons
         
-        switch weapons {
-        case "Sword":
-            strike = 10
-        case "Magic Stick":
-            magicPower = 5
-        case "Shield":
-            strike = 5
-        case "Axe":
-            strike = 25
-        default:
-            strike = 0
-            magicPower = 0
-        }
     }
     
 }
 
+// Class Weapons Initialization
+class AttackWeapons {
+    var strike = 0
+    
+}
 
-// Creating class Instances
-var warrior = Character(characterName: "", weapons: "Sword")
+class HealWeapons {
+    var heal = 0
+}
+
+
+// Creating class Character Instances
+var warrior = Character(characterName: "")
 warrior.life = 100
 
 
-var magus = Character(characterName: "", weapons: "Magic Stick")
+var magus = Character(characterName: "")
 magus.life = 80
 
 
-var colossus = Character(characterName: "", weapons: "Shield")
-colossus.life = 150
+var colossus = Character(characterName: "")
+colossus.life = 125
 
 
-var dwarf = Character(characterName: "", weapons: "Axe")
+var dwarf = Character(characterName: "")
 dwarf.life = 60
 
+// Creating Class AttackWeapons Instances
+var twoHandedSword = AttackWeapons()
+twoHandedSword.strike = 15
 
-// Creation of the array which will contain the team chosen by the player
-var team = [String: Character]()
+var twoHandedAxe = AttackWeapons()
+twoHandedAxe.strike = 20
 
-// Function that will create the Warrior
-func createWarrior() {
+var oneHandedMass = AttackWeapons()
+oneHandedMass.strike = 5
+
+var twoHandedStick = HealWeapons()
+twoHandedStick.heal = 8
+
+var magicWand = HealWeapons()
+magicWand.heal = 5
+
+var oneHandedSword = AttackWeapons()
+oneHandedSword.strike = 10
+
+var twoHandedMass = AttackWeapons()
+twoHandedMass.strike = 12
+
+
+
+// Creation of the dictionary which will contain the team chosen by the player
+var team1 = [String]()
+var team2 = [String]()
+
+// is Character's name unique name ?
+var uniqueName = ""
+var isItUniqueName = false
+
+
+// Creation of the variables which will containe the player's name
+var player1Name = ""
+var player2Name = ""
+
+// Functions that will create the Warrior
+func createWarriorTeam1(){
     if let name = readLine(){
         warrior.characterName = name
-        team[name] = warrior
+        team1.append(name)
+    }
+}
+
+func createWarriorTeam2(){
+    if let name = readLine(){
+        warrior.characterName = name
+        team2.append(name)
     }
 }
 
 // Function that will create the Magus
-func createMagus() {
+func createMagusTeam1(){
     if let name = readLine(){
         magus.characterName = name
-        team[name] = magus
+        team1.append(name)
+    }
+}
+
+func createMagusTeam2(){
+    if let name = readLine(){
+        magus.characterName = name
+        team2.append(name)
     }
 }
 
 // Fuction that will create the Colossus
-func createColossus(){
+func createColossusTeam1(){
     if let name = readLine(){
         colossus.characterName = name
-        team[name] = colossus
+        team1.append(name)
+    }
+}
+
+func createColossusTeam2(){
+    if let name = readLine(){
+        colossus.characterName = name
+        team2.append(name)
     }
 }
 
 // Function that will create the Dwarf
-func createDwarf(){
+func createDwarfTeam1(){
     if let name = readLine(){
         dwarf.characterName = name
-        team[name] = dwarf
+        team1.append(name)
+    }
+}
+
+func createDwarfTeam2(){
+    if let name = readLine(){
+        dwarf.characterName = name
+        team2.append(name)
     }
 }
 
@@ -113,8 +168,14 @@ func teamSelectionMenu3(){
         + "\n4. The Dwarf, does a lot of damage but is not very resistant.")
 }
 
-// Function that will create the player's team
-func createTeam() {
+// Function that will create the player one's team
+func createTeam1() {
+    
+    print("Player One, what's your name ?")
+    if let playerName = readLine(){
+        player1Name = playerName
+        print("Welcome to the batlle \(player1Name) !")
+    }
     
     teamSelectionMenu1()
     
@@ -122,19 +183,19 @@ func createTeam() {
         switch choice1 {
         case "1":
             print("You choose The Warior, what's his name ?")
-            createWarrior()
+            createWarriorTeam1()
             
         case "2":
             print("You choose The Magus, what's his name ?")
-            createMagus()
+            createMagusTeam1()
             
         case "3":
             print("You choose The Colossus, what's his name ?")
-            createColossus()
+            createColossusTeam1()
             
         case "4":
             print("You choose The Dwarf, what's his name ?")
-            createDwarf()
+            createDwarfTeam1()
             
         default:
             print("I don't understand.")
@@ -148,19 +209,19 @@ func createTeam() {
         switch choice2 {
         case "1":
             print("You choose The Warior, what's his name ?")
-            createWarrior()
+            createWarriorTeam1()
             
         case "2":
             print("You choose The Magus, what's his name ?")
-            createMagus()
+            createMagusTeam1()
             
         case "3":
             print("You choose The Colossus, what's his name ?")
-            createColossus()
+            createColossusTeam1()
             
         case "4":
             print("You choose The Dwarf, what's his name ?")
-            createDwarf()
+            createDwarfTeam1()
             
         default:
             print("I don't understand.")
@@ -174,19 +235,19 @@ func createTeam() {
         switch choice3 {
         case "1":
             print("You choose The Warior, what's his name ?")
-            createWarrior()
+            createWarriorTeam1()
             
         case "2":
             print("You choose The Magus, what's his name ?")
-            createMagus()
+            createMagusTeam1()
             
         case "3":
             print("You choose The Colossus, what's his name ?")
-            createColossus()
+            createColossusTeam1()
             
         case "4":
             print("You choose The Dwarf, what's his name ?")
-            createDwarf()
+            createDwarfTeam1()
             
         default:
             print("I don't understand.")
@@ -196,4 +257,92 @@ func createTeam() {
     
 }
 
-createTeam()
+func createTeam2(){
+    print("Player Two, what's your name ?")
+    if let playerName = readLine(){
+        player2Name = playerName
+        print("Welcome to the batlle \(player2Name) !")
+    }
+    
+    teamSelectionMenu1()
+    
+    if let choice1 = readLine() {
+        switch choice1 {
+        case "1":
+            print("You choose The Warior, what's his name ?")
+            createWarriorTeam2()
+            
+        case "2":
+            print("You choose The Magus, what's his name ?")
+            createMagusTeam2()
+            
+        case "3":
+            print("You choose The Colossus, what's his name ?")
+            createColossusTeam2()
+            
+        case "4":
+            print("You choose The Dwarf, what's his name ?")
+            createDwarfTeam2()
+            
+        default:
+            print("I don't understand.")
+            teamSelectionMenu1()
+        }
+    }
+    
+    teamSelectionMenu2()
+    
+    if let choice2 = readLine() {
+        switch choice2 {
+        case "1":
+            print("You choose The Warior, what's his name ?")
+            createWarriorTeam2()
+            
+        case "2":
+            print("You choose The Magus, what's his name ?")
+            createMagusTeam2()
+            
+        case "3":
+            print("You choose The Colossus, what's his name ?")
+            createColossusTeam2()
+            
+        case "4":
+            print("You choose The Dwarf, what's his name ?")
+            createDwarfTeam2()
+            
+        default:
+            print("I don't understand.")
+            teamSelectionMenu2()
+        }
+    }
+    
+    teamSelectionMenu3()
+    
+    if let choice3 = readLine() {
+        switch choice3 {
+        case "1":
+            print("You choose The Warior, what's his name ?")
+            createWarriorTeam2()
+            
+        case "2":
+            print("You choose The Magus, what's his name ?")
+            createMagusTeam2()
+            
+        case "3":
+            print("You choose The Colossus, what's his name ?")
+            createColossusTeam2()
+            
+        case "4":
+            print("You choose The Dwarf, what's his name ?")
+            createDwarfTeam2()
+            
+        default:
+            print("I don't understand.")
+            teamSelectionMenu3()
+        }
+    }
+}
+print("Hello fighters, and welcome to Warlords !")
+
+createTeam1()
+createTeam2()
